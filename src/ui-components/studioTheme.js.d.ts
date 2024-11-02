0459c3f49286a9c2334ea47591d861d32caf7f5a
gshen-uh <guoyang.shen@outlook.com>
2024-11-01 22:32:15 -0500

    bug fixes

--- src/ui-components/studioTheme.js.d.ts
+++ src/ui-components/studioTheme.js.d.ts
@@ -3,6 +3,6 @@
  * Please refrain from making any modifications to this file.              *
  * Any changes to this file will be overwritten when running amplify pull. *
  **************************************************************************/
-
+// @ts-expect-error Suppressing any type issue in auto-generated file
 declare const _default: any;
 export default _default;
