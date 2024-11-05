7bf391f218651edc78024587f091e9996a4ec85c
gshen-uh <guoyang.shen@outlook.com>
2024-11-05 00:17:36 -0600

    @ts-expect-error

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -1,5 +1,5 @@
 import { ModelInit, MutableModel, __modelMeta__, ManagedIdentifier } from "@aws-amplify/datastore";
-// @ts-ignore
+// @ts-expect-error
 import { LazyLoading, LazyLoadingDisabled } from "@aws-amplify/datastore";
 
 export enum VerificationStatus {
